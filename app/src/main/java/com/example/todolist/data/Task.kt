fc21d69e2cb81157d4761b8b203a4f19c73ac686
Feras Alnowiser <feras272@Ferass-MacBook-Air.local>
2022-02-24 16:53:41 +0300

    adding the task class and the parcelize and the room entity of s SQL lite

--- app/src/main/java/com/example/todolist/data/Task.kt
+++ app/src/main/java/com/example/todolist/data/Task.kt
@@ -2,16 +2,18 @@ package com.example.todolist.data
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.text.DateFormat
 
-@Entity
+@Entity(tableName = "task_table")
 @Parcelize
 data class Task (
     val name : String,
     val important : Boolean,
     val completed : Boolean,
-    val created : Long = System.currentTimeMillis()
+    val created : Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id : Int = 0
 ) : Parcelable {
     val createdDateFormatted: String
         get() = DateFormat.getDateTimeInstance().format(created)
